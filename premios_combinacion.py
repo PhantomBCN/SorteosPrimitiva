bf211cfb0275462b3b21b1f8014ab5bd13fd9ca3
PhantomBCN <davizaho@hotmail.com>
2023-03-31 09:43:28 +0200

    Añadidios los premiso de 5 + complementario y 6 + reintegro

--- premios_combinacion.py
+++ premios_combinacion.py
@@ -6,14 +6,18 @@ def comprobar_resultados(df_resultados, num1, num2, num3, num4, num5, num6, comp
                  names=['Fecha', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'Complementario', 'Reintegro'],
                  dtype=str, parse_dates=['Fecha'], dayfirst=True)
 '''
-    df_premios = pd.DataFrame(columns=['Fecha', 'Aciertos'])
+    df_premios = pd.DataFrame(columns=['Fecha', 'Aciertos' , 'Complementario', 'Reintegro'])
 
     aciertos=0
+    v_comp = 'N'
+    v_reint= 'N'
     
 
     for index, row in df_resultados.iterrows():
         # Extrae los valores de la combinación actual
         valores = row[['N1', 'N2', 'N3', 'N4', 'N5', 'N6']].tolist()
+        comp = row[['Complementario']].tolist()
+        reint= row[['Reintegro']].tolist()
         
         if str(num1).zfill(2) in valores:
             aciertos+=1
@@ -27,17 +31,27 @@ def comprobar_resultados(df_resultados, num1, num2, num3, num4, num5, num6, comp
             aciertos+=1
         if str(num6).zfill(2) in valores:
             aciertos+=1
-        df_premios.loc[len(df_premios)] = [row['Fecha'], aciertos]
+        if str(complementario).zfill(2) in comp:
+            v_comp = "S"
+        if str(reintegro) in reint:
+            v_reint = "S"
+        df_premios.loc[len(df_premios)] = [row['Fecha'], aciertos, v_comp, v_reint]
 
         aciertos=0
+        v_comp = 'N'
+        v_reint= 'N'
 
     dic_aciertos = {"0" : len(df_premios.query("Aciertos == 0")),
                    "1" : len(df_premios.query("Aciertos == 1")),
                    "2" : len(df_premios.query("Aciertos == 2")),
                    "3" : len(df_premios.query("Aciertos == 3")),
                    "4" : len(df_premios.query("Aciertos == 4")),
-                   "5" : len(df_premios.query("Aciertos == 5")),
-                   "6" : len(df_premios.query("Aciertos == 6"))}
+                   "5" : len(df_premios.query("Aciertos == 5 and Complementario == 'N'" )),
+                   "5C" : len(df_premios.query("Aciertos == 5 and Complementario == 'S'" )),
+                   "6" : len(df_premios.query("Aciertos == 6 and Reintegro == 'N'")),
+                   "6R" : len(df_premios.query("Aciertos == 6 and Reintegro == 'S'")),
+                   "Complementario" : len(df_premios.query("Complementario == 'S'")),
+                   "Reintegro" : len(df_premios.query("Reintegro == 'S'"))}
 
 
     return dic_aciertos
--- test.py
+++ test.py
@@ -1,7 +1,19 @@
+# Importar la biblioteca pandas para el manejo de datos en formato CSV
+import pandas as pd
+
+# Importar el módulo premios_combinacion que contiene la función comprobar_resultados
 import premios_combinacion
 
+# Leer el archivo CSV que contiene los resultados de la Primitiva
+df_resultados = pd.read_csv('SorteosPrimitiva_SinJOKER_CSV.csv', sep=';', header=None,
+                names=['Fecha', 'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'Complementario', 'Reintegro'],
+                dtype=str, parse_dates=['Fecha'], dayfirst=True)
+
+# Crear un diccionario vacío para almacenar los resultados de la función comprobar_resultados
 resultado={}
 
-resultado = premios_combinacion.comprobar_resultados(1, 15, 22, 33, 38, 44, 2)
+# Llamar a la función comprobar_resultados y pasar los números del sorteo como argumentos
+resultado = premios_combinacion.comprobar_resultados(df_resultados, 3, 9, 12, 14, 26, 44, 17, 2)
 
+# Imprimir los resultados
 print(resultado)
